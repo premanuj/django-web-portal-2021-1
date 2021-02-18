0db5e63d8889c37ccb46582377485f1bb4919505
premanuj <mesubedianuj@gmail.com>
2021-02-18 07:53:29 +0545

    User model customization

--- portal/settings.py
+++ portal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
